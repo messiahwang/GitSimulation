8530f1abfac6a7adaccbed17d094a8c308b87b07
DavidMah <Mahhaha@gmail.com>
2012-02-01 22:32:59 -0800

    FileSystem references work
    - . points to self
    - .. points to parent

--- commandline.coffee
+++ commandline.coffee
@@ -1,19 +1,69 @@
 $(document).ready(() ->
-  prepareVisualConsole()
   prepareFileSystem()
   prepareKeyListener()
-  setCommands()
+  prepareVisualConsole()
 )
 
-prepareVisualConsole = () ->
-  window.current_input = ""
+# --------------- File System Stuff -------------
 
+# A file system object is a directory or a file
+# that is indicated by the _type key
+# a file has text
 prepareFileSystem = () ->
   window.file_system =
-    home:
-       bob: {}
+    '/':
+      _type: 'directory'
+      _entries:['.', '..', 'home']
+      home:
+        _type: 'directory'
+        _entries:['.', '..', 'bob']
+        bob:
+          _type: 'directory'
+          _entries:['.', '..', 'a', 'b', 'c']
+          a:
+            _type: 'file'
+          b:
+            _type: 'file'
+          c:
+            _type: 'file'
+  window.current_location = "/home/bob"
+  # Makes all . point to the same directory
+  # and makes all .. point to the parent directory
+  crawl(window.file_system['/'], '/', (dir, dirname, entry) ->
+    if entry == '.'
+      dir[entry] = dir
+    if dir[entry]['_type'] == 'directory'
+      dir[entry]['..'] = dir
+  )
+
+
+# Traverse the entire file system, applying the
+# given action function to all items
+crawl = (current_dir = window.file_system['/'], current_name, action = null) ->
+  entries = current_dir['_entries']
+  for entry in entries
+    console.log(entry)
+    action(current_dir, current_name, entry) if action != null
+    continue_crawl = current_dir[entry] != undefined and current_dir[entry]['_type'] == 'directory'
+    continue_crawl = continue_crawl and current_dir[entry] != current_dir
+    continue_crawl = continue_crawl and current_dir[entry][current_name] != current_dir
+    crawl(current_dir[entry], entry, action) if continue_crawl
+
+window.crawl = crawl
+
+
+accessDirectory = (pathname) ->
+  paths = pathname.split("/")
+  paths.shift()
+  current_spot = window.file_system['/']
+  for path in paths
+    current_spot = current_spot[path]
+  current_spot
 
 # --------------- User Input Stuff -------------
+prepareVisualConsole = () ->
+  window.current_input = ""
+  updateCurrentInput()
 
 prepareKeyListener = () ->
   $(document).keypress((e) ->
@@ -36,11 +86,11 @@ handleKeyPress = (e) ->
 
 injectToInput = (character) ->
   window.current_input += character
-  console.log(character)
   updateCurrentInput()
 
 updateCurrentInput = () ->
-  $('#tl20').text(window.current_input)
+  ps1 = window.current_location
+  $('#tl20').text("#{ps1}$ #{window.current_input}")
 
 handleReservedKey = (keyCode) ->
   RESERVED_KEYS[keyCode]()
@@ -65,17 +115,29 @@ window.reserved = RESERVED_KEYS
 ## -------- End of User Input Stuff --------------
 
 #-------------- Command stuff
+print = (message) ->
+  bottom = $('#tl19')
+  bottom.text(bottom.text() + message)
+
 runCommand = (command) ->
   shiftOutput()
-  $('#tl19').text(command)
+  command = COMMANDS[command]
+  command() if(command != undefined)
+
+runCommandls = () ->
+  current_dir = window.current_location
+  dir = accessDirectory(current_dir)
+  for entry in dir['_entries']
+    console.log(entry)
+    print("#{entry} ")
+
+COMMANDS =
+  ls: runCommandls
 
 shiftOutput = () ->
   for i in [1..18]
     do (i) ->
       previous = $("#tl#{i + 1}").text()
-      console.log(previous)
       $("#tl#{i}").text(previous)
-
-setCommands = () ->
-
+  $('#tl19').text("")
 
--- commandline.js
+++ commandline.js
@@ -1,20 +1,82 @@
 (function() {
-  var RESERVED_KEYS, handleKeyPress, handleReservedKey, injectToInput, inputBackspace, inputEnter, prepareFileSystem, prepareKeyListener, prepareVisualConsole, runCommand, setCommands, shiftOutput, updateCurrentInput;
+  var COMMANDS, RESERVED_KEYS, accessDirectory, crawl, handleKeyPress, handleReservedKey, injectToInput, inputBackspace, inputEnter, prepareFileSystem, prepareKeyListener, prepareVisualConsole, print, runCommand, runCommandls, shiftOutput, updateCurrentInput;
   $(document).ready(function() {
-    prepareVisualConsole();
     prepareFileSystem();
     prepareKeyListener();
-    return setCommands();
+    return prepareVisualConsole();
   });
-  prepareVisualConsole = function() {
-    return window.current_input = "";
-  };
   prepareFileSystem = function() {
-    return window.file_system = {
-      home: {
-        bob: {}
+    window.file_system = {
+      '/': {
+        _type: 'directory',
+        _entries: ['.', '..', 'home'],
+        home: {
+          _type: 'directory',
+          _entries: ['.', '..', 'bob'],
+          bob: {
+            _type: 'directory',
+            _entries: ['.', '..', 'a', 'b', 'c'],
+            a: {
+              _type: 'file'
+            },
+            b: {
+              _type: 'file'
+            },
+            c: {
+              _type: 'file'
+            }
+          }
+        }
       }
     };
+    window.current_location = "/home/bob";
+    return crawl(window.file_system['/'], '/', function(dir, dirname, entry) {
+      if (entry === '.') {
+        dir[entry] = dir;
+      }
+      if (dir[entry]['_type'] === 'directory') {
+        return dir[entry]['..'] = dir;
+      }
+    });
+  };
+  crawl = function(current_dir, current_name, action) {
+    var continue_crawl, entries, entry, _i, _len, _results;
+    if (current_dir == null) {
+      current_dir = window.file_system['/'];
+    }
+    if (action == null) {
+      action = null;
+    }
+    entries = current_dir['_entries'];
+    _results = [];
+    for (_i = 0, _len = entries.length; _i < _len; _i++) {
+      entry = entries[_i];
+      console.log(entry);
+      if (action !== null) {
+        action(current_dir, current_name, entry);
+      }
+      continue_crawl = current_dir[entry] !== void 0 && current_dir[entry]['_type'] === 'directory';
+      continue_crawl = continue_crawl && current_dir[entry] !== current_dir;
+      continue_crawl = continue_crawl && current_dir[entry][current_name] !== current_dir;
+      _results.push(continue_crawl ? crawl(current_dir[entry], entry, action) : void 0);
+    }
+    return _results;
+  };
+  window.crawl = crawl;
+  accessDirectory = function(pathname) {
+    var current_spot, path, paths, _i, _len;
+    paths = pathname.split("/");
+    paths.shift();
+    current_spot = window.file_system['/'];
+    for (_i = 0, _len = paths.length; _i < _len; _i++) {
+      path = paths[_i];
+      current_spot = current_spot[path];
+    }
+    return current_spot;
+  };
+  prepareVisualConsole = function() {
+    window.current_input = "";
+    return updateCurrentInput();
   };
   prepareKeyListener = function() {
     $(document).keypress(function(e) {
@@ -39,11 +101,12 @@
   };
   injectToInput = function(character) {
     window.current_input += character;
-    console.log(character);
     return updateCurrentInput();
   };
   updateCurrentInput = function() {
-    return $('#tl20').text(window.current_input);
+    var ps1;
+    ps1 = window.current_location;
+    return $('#tl20').text("" + ps1 + "$ " + window.current_input);
   };
   handleReservedKey = function(keyCode) {
     return RESERVED_KEYS[keyCode]();
@@ -66,22 +129,44 @@
     13: inputEnter
   };
   window.reserved = RESERVED_KEYS;
+  print = function(message) {
+    var bottom;
+    bottom = $('#tl19');
+    return bottom.text(bottom.text() + message);
+  };
   runCommand = function(command) {
     shiftOutput();
-    return $('#tl19').text(command);
+    command = COMMANDS[command];
+    if (command !== void 0) {
+      return command();
+    }
   };
-  shiftOutput = function() {
-    var i, _results;
+  runCommandls = function() {
+    var current_dir, dir, entry, _i, _len, _ref, _results;
+    current_dir = window.current_location;
+    dir = accessDirectory(current_dir);
+    _ref = dir['_entries'];
     _results = [];
-    for (i = 1; i <= 18; i++) {
-      _results.push((function(i) {
-        var previous;
-        previous = $("#tl" + (i + 1)).text();
-        console.log(previous);
-        return $("#tl" + i).text(previous);
-      })(i));
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      entry = _ref[_i];
+      console.log(entry);
+      _results.push(print("" + entry + " "));
     }
     return _results;
   };
-  setCommands = function() {};
+  COMMANDS = {
+    ls: runCommandls
+  };
+  shiftOutput = function() {
+    var i, _fn;
+    _fn = function(i) {
+      var previous;
+      previous = $("#tl" + (i + 1)).text();
+      return $("#tl" + i).text(previous);
+    };
+    for (i = 1; i <= 18; i++) {
+      _fn(i);
+    }
+    return $('#tl19').text("");
+  };
 }).call(this);
